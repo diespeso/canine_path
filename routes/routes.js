881fed90ed0fadabd9edc00da81d459cb6068f83
diespeso <regalado.correo@gmail.com>
2022-03-27 17:17:37 -0700

    started working on search engine

--- routes/routes.js
+++ routes/routes.js
@@ -10,10 +10,36 @@ const { append } = require('express/lib/response');
 module.exports = router;
 
 const config = require('../config');
+const { createConnection } = require('net');
+const { format } = require('path');
 
-router.get('/buscador_perros', (req, res) => {
-    console.log(req.body)
-    res.render('buscador_perros', {});
+router.get('/buscador_perros', (req, res) => { 
+    var con = mysql.createConnection(config.db_con);
+    con.connect();
+    con.query('USE canine_path;')
+    con.query('select * from perro; select race, count(race) as count from perro group by race;', (err, rows, fields) => {
+        var forma_razas = "";
+        var razas = rows[1];
+        var perros = rows[0];
+        var result = "";
+        for(var i = 0; i < perros.length; i++) {
+            result += `<a href="perro/${perros[i].id}" class="dog_container_clicker"><div class="dog_container">
+            <img class="dog_pic" src="/img/dog_profiles/${perros[i].id}.jpg">
+            <h1>${perros[i].name}</h1>
+        </div></a>`;
+        }
+        for(var i = 0; i < razas.length; i++) {
+            forma_razas += `<option value="${razas[i].race}">${razas[i].race}(${razas[i].count})</option>`
+        }
+        res.render('buscador_perros', {
+            contenido: {
+                forma: forma_razas,
+                perritos: result
+                
+            }
+        });
+    })
+    
 })
 
 router.get('/', (req, res) => {
@@ -109,28 +135,76 @@ function gen_dog_card(dog_id) {
 
 router.post('/perros', (req, res) => {
     console.log(req.body);
+    var query = "select * from perro inner join personality on perro.id = personality.id_perro ";
+    var forma = req.body;
+    var count = 0;
+    if(forma.f_sexo != 'na') {
+        query += ` where sex = '${forma.f_sexo}'`
+        count++;
+    }
+
+    if(forma.f_tamano != 'na') {
+        //use ranges for this one
+        var min = 0
+        var max = 3.0
+        if(forma.f_tamano == "S") {
+            max = 0.5 //60cms max
+        } else if(forma.f_tamano == "M") {
+            min = 0.5
+            max = 0.7
+        } else {
+            min = 0.7
+        }
+
+        if(count > 0) { //if a 'and is needed'
+            query += ` and size > ${min} and size < ${max} `
+        } else {
+            query += ` where size > ${min} and size < ${max} `
+        }
+        count++;
+    }
+
+    if(forma.f_raza != 'na') {
+        if(count > 0) {
+            query += ` and race = '${forma.f_raza}' `
+        } else {
+            query += ` where race = '${forma.f_raza}' `
+        }
+        count++;
+    }
+
+
+    //TODO: AGREGAR BUSQUEDAS POR ASPECTOS DE PERSONALIDAD
+
+
+    console.log(query);
+    
     var grid = gen_dog_grid();
     //
-    var con = mysql.createConnection({
-        host: 'localhost',
-        user: 'root'
-    });
+    var con = mysql.createConnection(config.db_con);
     con.connect();
     con.query('USE canine_path;');
-    con.query(`SELECT * FROM perro;`, function(err, rows, fields) {
+    con.query(`${query}; select race, count(race) as count from perro group by race;`, function(err, rows, fields) {
         if(err) throw err;
+        var perros = rows[0];
+        var razas = rows[1];
         var result = "";
-        for(var i = 0; i < rows.length; i++) {
-            result += `<a href="perro/${rows[i].id}" class="dog_container_clicker"><div class="dog_container">
-            <img class="dog_pic" src="/img/dog_profiles/${rows[i].id}.jpg">
-            <h1>${rows[i].name}</h1>
+        for(var i = 0; i < perros.length; i++) {
+            result += `<a href="perro/${perros[i].id}" class="dog_container_clicker"><div class="dog_container">
+            <img class="dog_pic" src="/img/dog_profiles/${perros[i].id}.jpg">
+            <h1>${perros[i].name}</h1>
         </div></a>`;
+        }
+        var forma_razas = "";
+        for(var i = 0; i < razas.length; i++) {
+            forma_razas += `<option value="${razas[i].race}">${razas[i].race}(${razas[i].count})</option>`
         }
         res.render('buscador_perros', {
             contenido: {
                 old: req.body,
                 //perritos: grid
-                perritos: result
+                perritos: result,
+                forma: forma_razas
     
             }
         })
@@ -156,7 +230,6 @@ router.post('/perros', (req, res) => {
 
 //perfil individual del perro
 router.get('/perro/:id', (req, res) => {
-    console.log(req.params.id)
     var con = mysql.createConnection(config.db_con);
     con.query('use canine_path;');
     con.query(`select *, perro.name as perro_name,
@@ -168,7 +241,6 @@ router.get('/perro/:id', (req, res) => {
     inner join personality
     on personality.id_perro = perro.id
     where perro.id = ${req.params.id};`, function(err, rows, fields) {
-        console.log(rows[0]);
         var perfil = {};
         perfil.perro_id = rows[0].perro_id;
         perfil.availability = rows[0].availability;
@@ -203,53 +275,41 @@ router.get('/perro/:id', (req, res) => {
         refugio.address = rows[0].address;
         refugio.city = rows[0].city;
         refugio.country = rows[0].country;
-        console.log("ffff");
-        console.log(refugio);
         res.render('perfil_perro', {perfil: perfil, refugio: refugio})
     }); 
     con.end();
 }) 
 
 router.get('/refugio/:id', (req, res) => {
-    console.log(req.params.id);
     var con = mysql.createConnection(config.db_con);
     con.query('use canine_path;');
-    /*con.query(`select *, perro.id as perro_id, perro.name as perro_name
-        from refugio
-        inner join perro
-            on refugio.id = perro.id_refugio
-        where refugio.id = ${req.params.id};`, (err, rows, fields) => {
-            console.log(rows);
-
-            res.render('perfil_refugio', {});
-        });*/
-        con.query(`SELECT COUNT(IF(availability = 'ADOPTABLE', 1, NULL)) 'adoptables',
-        COUNT(IF(availability = 'ADOPTADO', 1, NULL)) 'adoptados',
-        COUNT(IF(availability = 'NO DISPONIBLE', 1, NULL)) 'no_disponibles'
-        FROM perro
-        WHERE perro.id_refugio = ${req.params.id};
+    con.query(`SELECT COUNT(IF(availability = 'ADOPTABLE', 1, NULL)) 'adoptables',
+    COUNT(IF(availability = 'ADOPTADO', 1, NULL)) 'adoptados',
+    COUNT(IF(availability = 'NO DISPONIBLE', 1, NULL)) 'no_disponibles'
+    FROM perro
+    WHERE perro.id_refugio = ${req.params.id};
+    
+    SELECT * FROM perro WHERE id_refugio = ${req.params.id};
+    
+    SELECT * FROM refugio WHERE id = ${req.params.id};`, (err, rows, fields) => {
         
-        SELECT * FROM perro WHERE id_refugio = ${req.params.id};
-        
-        SELECT * FROM refugio WHERE id = ${req.params.id};`, (err, rows, fields) => {
-            
-            var resumen = rows[0];
-            var perros = rows[1];
+        var resumen = rows[0];
+        var perros = rows[1];
 
-            var perritos = "";
-            for(var i = 0; i < perros.length; i++) {
-                perritos += `<a href="../perro/${perros[i].id}" class="dog_container_clicker"><div class="dog_container">
-                <img class="dog_pic" src="/img/dog_profiles/${perros[i].id}.jpg">
-                <h1>${perros[i].name}</h1>
-            </div></a>`;
-            }
+        var perritos = "";
+        for(var i = 0; i < perros.length; i++) {
+            perritos += `<a href="../perro/${perros[i].id}" class="dog_container_clicker"><div class="dog_container">
+            <img class="dog_pic" src="/img/dog_profiles/${perros[i].id}.jpg">
+            <h1>${perros[i].name}</h1>
+        </div></a>`;
+        }
 
-            res.render('perfil_refugio', {
-                resumen: resumen[0],
-                perritos: perritos,
-                refugio: rows[2][0]
-            });
+        res.render('perfil_refugio', {
+            resumen: resumen[0],
+            perritos: perritos,
+            refugio: rows[2][0]
         });
+    });
     con.end();
 });
  
